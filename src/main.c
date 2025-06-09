1d3e1535532eeece8fa1272d5f5cac9f713330a5
Semyon <zhigunovsemen@gmail.com>
2025-06-09 17:37:19 +0300

    Открываю сокет

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+/*
+ * 7. Определить параметры соединения – номера портов с обеих сторон.
+ */
+
+typedef int fd_t;
 
 int main ()
 {
-	puts("Hello World!");
+	fd_t sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		perror("Failed to open sock");
+		return 1;
+	}
+	printf("Открытый сокет: %d\n", sock);
+
+	close(sock);
 	return 0;
 }
