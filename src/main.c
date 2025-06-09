f23a23b6d08f8aab33c2e3e9468e7b4a7928ab10
Semyon <zhigunovsemen@gmail.com>
2025-06-09 19:27:38 +0300

    Биндаю(?) сокет

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -8,15 +10,33 @@
 
 typedef int fd_t;
 
-int main ()
+constexpr in_port_t PORT = 8789;
+constexpr uint32_t LOCALHOST = (127 << 24) + 1;
+
+int main()
 {
-	fd_t sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
+	// Структура с адресом и портом
+	struct sockaddr_in sockaddr_ = {};
+	sockaddr_.sin_family = AF_INET;
+	sockaddr_.sin_port = PORT;
+	sockaddr_.sin_addr = (struct in_addr){htonl(LOCALHOST)};
+
+	// Входящий сокет
+	fd_t inc_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (inc_sock == -1) {
 		perror("Failed to open sock");
 		return 1;
 	}
-	printf("Открытый сокет: %d\n", sock);
+	printf("Открытый сокет: %d\n", inc_sock);
 
-	close(sock);
+	// Соединение сокета с портом и адресом
+	if (-1 == bind(inc_sock, (struct sockaddr const *)&sockaddr_,
+		       sizeof(sockaddr_))) {
+		perror("Failed to bind socket");
+		close(inc_sock);
+		return 1;
+	}
+
+	close(inc_sock);
 	return 0;
 }
