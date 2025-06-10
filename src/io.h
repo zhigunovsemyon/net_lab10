ff32a17b8b03b0dd1f08cd119b25ed8463abb26b
Semyon <zhigunovsemen@gmail.com>
2025-06-10 13:42:43 +0300

    Выкинул io

--- src/io.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_H_
-#define IO_H_
-
-/*Чтение базы данных из текстового файла в ОЗУ.
- *!ПАМЯТЬ ДОЛЖНА БЫТЬ ОСВОБОЖДЕНА ВЫЗОВОМ free()!*/
-char * read_db_from_file(char const * fname);
-
-#endif // !IO_H_
